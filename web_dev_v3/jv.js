c4ab076a873144d296b00eadc9882601f9553fe8
thee-falcon <omar.makran.10@gmail.com>
2024-02-03 16:42:27 +0100

    save my work on web dev

---
+++ web_dev_v3/jv.js
@@ -0,0 +1,10 @@
+const myHomeCity = "Al-Hoceima";
+const MyHomeState = "Agadir";
+const myHomeCountry = "Morocco";
+
+function logOutYourHome(city, state, country)
+{
+	console.log(`You are from ${city} ${state} ${country}`);
+}
+
+logOutYourHome(myHomeCity, MyHomeState, myHomeCountry);
